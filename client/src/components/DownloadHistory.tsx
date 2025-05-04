3c899edf1ab74365822f332637593afc277aa7a2
windzer99311 <windzergamer@gmail.com>
2025-05-05 00:36:51 +0530

    Update DownloadHistory.tsx

--- client/src/components/DownloadHistory.tsx
+++ client/src/components/DownloadHistory.tsx
@@ -119,6 +119,18 @@ export default function DownloadHistory() {
         </Button>
       </div>
       
+      <div className="bg-yellow-50 dark:bg-yellow-900/20 border border-yellow-200 dark:border-yellow-700 rounded-lg p-3 mb-4 text-sm text-yellow-800 dark:text-yellow-200">
+        <div className="flex items-center">
+          <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-2 flex-shrink-0" viewBox="0 0 20 20" fill="currentColor">
+            <path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2h-1V9a1 1 0 00-1-1z" clipRule="evenodd" />
+          </svg>
+          <div>
+            <p className="font-medium">Downloads are temporary</p>
+            <p className="mt-1">Please download your files immediately as they will be automatically removed when the server restarts or after periods of inactivity.</p>
+          </div>
+        </div>
+      </div>
+      
       <div className="overflow-hidden bg-background rounded-lg">
         {downloads.map((download) => (
           <div key={download.id} className="flex flex-col sm:flex-row items-start sm:items-center gap-4 p-4 border-b border-secondary last:border-b-0">
