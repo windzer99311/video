53de2b6eb6d30764b8bcc2c91c9095a754ae01d4
windzer99311 <windzergamer@gmail.com>
2025-05-05 00:50:46 +0530

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,7 @@
 import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
 import { setupVite, serveStatic, log } from "./vite";
+import { setupDatabase } from "../db/setup";
 
 const app = express();
 app.use(express.json());
@@ -37,6 +38,9 @@ app.use((req, res, next) => {
 });
 
 (async () => {
+  // Setup database tables when the server starts
+  await setupDatabase();
+  
   const server = await registerRoutes(app);
 
   app.use((err: any, _req: Request, res: Response, _next: NextFunction) => {
